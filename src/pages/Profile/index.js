39f3dbe6c8d24f60ee7a172c6099c46064aae829
Mohammad Aasif <kasifsaif784@gmail.com>
2023-11-09 15:03:55 +0530

    add icon

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -12,12 +12,12 @@ const Profile = () => {
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state.orebiReducer);
   const { userInfo } = useSelector((state) => state.orebiReducer);
-    console.log(userInfo)
+  console.log(userInfo);
   useEffect(() => {
-    if (userInfo.length ===0 ) {
+    if (userInfo.length === 0) {
       navigate("/signin");
     }
-  }, [navigate ,userInfo]);
+  }, [navigate, userInfo]);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -27,11 +27,7 @@ const Profile = () => {
 
   const [successMsg, setSuccessMsg] = useState("");
 
-  // const textTrim = (text, value) => {
-  //   const words = text.split(" ");
-  //   const firstTwoWords = words.slice(0, value).join(" ");
-  //   return firstTwoWords;
-  // };
+
   const handleEmail = (e) => {
     setEmail(e.target.value);
     setErrEmail("");
@@ -60,10 +56,10 @@ const Profile = () => {
       setPassword("");
     }
   };
-  const handleLogout =()=>{
-    dispatch(removeUserInfo())
+  const handleLogout = () => {
+    dispatch(removeUserInfo());
     navigate("/");
-  }
+  };
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="w-1/2 hidden lgl:inline-flex h-full text-white">
@@ -134,9 +130,23 @@ const Profile = () => {
                   <button
                     onClick={handleLogout}
                     type="button"
-                    className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+                    className="text-white flex items-center justify-between bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
                   >
-                   
+                    <svg
+                      className="w-6 h-6 dark:text-white mr-3"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"
+                      ></path>
+                    </svg>
                     Log Out
                   </button>
                 </div>
